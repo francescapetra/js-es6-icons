8e05dea4c98fd11cabe3297651a7ecb4a7ad5e87
francescapetra <francescalaurapetra@gmail.com>
2021-03-23 15:35:21 +0100

    cirlo foreach e append

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 // Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-// 
-// Milestone 2
-// Coloriamo le icone per tipo
-// 
-// Milestone 3
-// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
-
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibil come //da layout.
 const icons = [
   {
     name: 'apple-alt',
@@ -117,3 +110,22 @@ const icons = [
     category: "animal"
   },
 ];
+//faccio ciclare gli elementi
+// destrutturo e vado ad appendere
+icons.forEach((icon) => {
+
+  const {name,prefix,family} = icon;
+
+  const html = `<div>
+    <i class="${family} ${prefix} ${name}"></i>
+    <div class ="title">${name}</div>
+  </div>`;
+
+  $(".icons").append(html);
+});
+
+// Milestone 2
+// Coloriamo le icone per tipo
+//
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
